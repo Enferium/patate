d643e85c1b230c28969b79fab28368e80d7b60f1
Thomas Westermann <thomas.westermann@orange.fr>
2016-05-06 15:10:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
-echo "lancement java"
+echo "Lancement java"
 cd JavaProlog
-java -classpath /usr/local/sicstus4.3.2/lib/sicstus-4.3.2/bin/jasper.jar:/usr/local/sicstus4.3.2/lib/sicstus-4.3.2/bin/prologbeans.jar:. Main 5555&
+java -classpath /applis/sicstus-4.3.2/lib/sicstus-4.3.2/bin/jasper.jar:/applis/sicstus-4.3.2/lib/sicstus-4.3.2/bin/prologbeans.jar:. Main $4&
 sleep 1
-echo "lancement Joueur "
+echo "Lancement Joueur"
 cd ..
-./ServeurC/joueurTicTacToe 127.0.0.1 5000 mathieu 5555
+./ServeurC/joueurTicTacToe $1 $2 $3 $4
